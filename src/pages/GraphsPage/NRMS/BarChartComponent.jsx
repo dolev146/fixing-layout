0420a4a6bd7e9ab957274194bd7a68f8366cbb56
ddublon <127298010+ddublon@users.noreply.github.com>
2023-05-15 16:27:13 +0300

    just need to add psd

--- src/pages/GraphsPage/NRMS/BarChartComponent.jsx
+++ src/pages/GraphsPage/NRMS/BarChartComponent.jsx
@@ -6,64 +6,78 @@ import AOBarChart from "./AOBarChart";
 
 
 
-const BarChartComponent = () => {
-  const isAbleDeleteRms = false;
-  const [siteDepthArray, setSiteDepthArray] = useState([]);
-  const [isAnimating, setIsAnimating] = useState(false);
-  const [rmsData, setRmsData] = useState([]);
-  const [shouldUpdate, setShouldUpdate] = useState(true);
-  const [timeoutId, setTimeoutId] = useState(null);
-  const [currentDepth, setCurrentDepth] = useState(5);
-  const marginTop = 0;
-  const height = 200;
-  const direction = "rtl";
-  const max = 15;
-  const min = -3;
-  const max_nrms = 4;
-  const handleDeleteFn = () => {};
+const BarChartComponent = (
+  {
+    isAnimating,
+    siteDepthArray,
+    setSiteDepthArray,
+    rmsData,
+    setRmsData,
+    shouldUpdate,
+    setShouldUpdate,
+    timeoutId,
+    setTimeoutId,
+    currentDepth,
+    setCurrentDepth,
+  }
+ ) => {
+  // const isAbleDeleteRms = false;
+  // const [isAnimating, setIsAnimating] = useState(false);
+  // const [siteDepthArray, setSiteDepthArray] = useState([]);
+  // const [rmsData, setRmsData] = useState([]);
+  // const [shouldUpdate, setShouldUpdate] = useState(true);
+  // const [timeoutId, setTimeoutId] = useState(null);
+  // const [currentDepth, setCurrentDepth] = useState(5);
+  // const marginTop = 0;
+  // const height = 200;
+  // const direction = "rtl";
+  // const max = 15;
+  // const min = -3;
+  // const max_nrms = 4;
+  // const handleDeleteFn = () => {};
 
-  const generateData = (depth = currentDepth) => {
-    if (depth < 0 || !shouldUpdate) {
-      return;
-    }
+  // const generateData = (depth = currentDepth) => {
+  //   if (depth < 0 || !shouldUpdate) {
+  //     return;
+  //   }
 
-    setCurrentDepth(depth);
+  //   setCurrentDepth(depth);
 
-    setSiteDepthArray((prevSiteDepthArray) => [
-      ...prevSiteDepthArray,
-      depth.toFixed(5),
-    ]);
-    let rmsValue;
-    if (depth < 5) {
-      rmsValue = 2 + Math.random();
-    } else {
-      rmsValue = Math.random();
-    }
-    setRmsData((prevRmsData) => [...prevRmsData, rmsValue.toFixed(5)]);
-    if (shouldUpdate) {
-      const newTimeoutId = setTimeout(() => {
-        generateData(depth - 0.1);
-      }, 5000 / ((15 - 0) * 10));
-      setTimeoutId(newTimeoutId);
-    }
-  };
+  //   setSiteDepthArray((prevSiteDepthArray) => [
+  //     ...prevSiteDepthArray,
+  //     depth.toFixed(5),
+  //   ]);
+  //   let rmsValue;
+  //   if (depth < 5) {
+  //     rmsValue = 2 + Math.random();
+  //   } else {
+  //     rmsValue = Math.random();
+  //   }
+  //   setRmsData((prevRmsData) => [...prevRmsData, rmsValue.toFixed(5)]);
+  //   if (shouldUpdate) {
+  //     const newTimeoutId = setTimeout(() => {
+  //       generateData(depth - 0.1);
+  //     }, 5000 / ((15 - 0) * 10));
+  //     setTimeoutId(newTimeoutId);
+  //   }
+  // };
 
-  const startRmsAnimation = () => {
-    console.log("startRmsAnimation");
-    setIsAnimating(!isAnimating);
-    setShouldUpdate(!shouldUpdate);
-    if (shouldUpdate) {
-      generateData();
-    } else {
-      clearTimeout(timeoutId);
-    }
-  };
+  // const startRmsAnimation = () => {
+  //   console.log("startRmsAnimation");
+  //   setIsAnimating(!isAnimating);
+  //   setShouldUpdate(!shouldUpdate);
+  //   if (shouldUpdate) {
+  //     generateData();
+  //   } else {
+  //     clearTimeout(timeoutId);
+  //   }
+  // };
 
   return (
     <div>
-      <button onClick={() => startRmsAnimation()}>
+      {/* <button onClick={() => startRmsAnimation()}>
         {isAnimating ? "Stop RMS Animation" : "Start RMS Animation"}{" "}
-      </button>
+      </button> */}
       <div
         style={{
           width: 200,
@@ -73,16 +87,16 @@ const BarChartComponent = () => {
         }}
       >
         <AOBarChart
-          isAbleDeleteRms={isAbleDeleteRms}
+          isAbleDeleteRms={false}
           siteDepthArray={siteDepthArray}
           rmsData={rmsData}
-          direction={direction}
-          marginTop={marginTop}
-          height={height}
-          max={max}
-          min={min}
-          max_nrms={max_nrms}
-          handleDeleteFn={handleDeleteFn}
+          direction={"rtl"}
+          marginTop={0}
+          height={200}
+          max={15}
+          min={-3}
+          max_nrms={4}
+          handleDeleteFn={() => {}}
         />
       </div>
     </div>
--- src/pages/GraphsPage/RMSPSDContainer.jsx
+++ src/pages/GraphsPage/RMSPSDContainer.jsx
@@ -5,12 +5,79 @@ import PSDComponent from "./PSD/PSDComponent";
 const RMSPSDContainer = () => {
   const [isAnimating, setIsAnimating] = useState(false);
 
+  // RMS related code
+  const [siteDepthArray, setSiteDepthArray] = useState([]);
+  const [rmsData, setRmsData] = useState([]);
+  const [shouldUpdate, setShouldUpdate] = useState(true);
+  const [timeoutId, setTimeoutId] = useState(null);
+  const [currentDepth, setCurrentDepth] = useState(5);
+  const generateData = (depth = currentDepth) => {
+    if (depth < 0 || !shouldUpdate) {
+      return;
+    }
+
+    setCurrentDepth(depth);
+
+    setSiteDepthArray((prevSiteDepthArray) => [
+      ...prevSiteDepthArray,
+      depth.toFixed(5),
+    ]);
+    let rmsValue;
+    if (depth < 5) {
+      rmsValue = 2 + Math.random();
+    } else {
+      rmsValue = Math.random();
+    }
+    setRmsData((prevRmsData) => [...prevRmsData, rmsValue.toFixed(5)]);
+    if (shouldUpdate) {
+      const newTimeoutId = setTimeout(() => {
+        generateData(depth - 0.1);
+      }, 5000 / ((15 - 0) * 10));
+      setTimeoutId(newTimeoutId);
+    }
+  };
+  const startRmsAnimation = () => {
+    console.log("startRmsAnimation");
+    setIsAnimating(!isAnimating);
+    setShouldUpdate(!shouldUpdate);
+    if (shouldUpdate) {
+      generateData();
+    } else {
+      clearTimeout(timeoutId);
+    }
+  };
+
   return (
     <div>
-      <button onClick={() => {setIsAnimating(!isAnimating)}}>{isAnimating ? "stop" : "start"}</button>
+      <button
+        onClick={() => {
+          setIsAnimating(!isAnimating);
+          setShouldUpdate(!shouldUpdate);
+          if (shouldUpdate) {
+            generateData();
+          } else {
+            clearTimeout(timeoutId);
+          }
+        }}
+      >
+        {isAnimating ? "stop" : "start"}
+      </button>
       <div className="flex">
-        <BarChartComponent />
-        <PSDComponent />
+        <BarChartComponent
+          isAnimating={isAnimating}
+          siteDepthArray={siteDepthArray}
+          setSiteDepthArray={setSiteDepthArray}
+          rmsData={rmsData}
+          setRmsData={setRmsData}
+          shouldUpdate={shouldUpdate}
+          setShouldUpdate={setShouldUpdate}
+          timeoutId={timeoutId}
+          setTimeoutId={setTimeoutId}
+          currentDepth={currentDepth}
+          setCurrentDepth={setCurrentDepth}
+          startRmsAnimation={startRmsAnimation}
+        />
+        <PSDComponent isAnimating={isAnimating} />
       </div>
     </div>
   );
